d6a1da4adaa89a358aa6fb4c865aad1a23469e1f
Gaël Berthaud-Müller <blacksponge@tuta.io>
2018-10-17 10:39:28 +0200

    Add new console component

--- client/app/app-routing.module.ts
+++ client/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GamesComponent } from './games/games.component';
 import { GameDetailComponent } from './game-detail/game-detail.component';
+import { ConsoleComponent } from './console/console.component';
 
 const routes: Routes = [
   {path: "games", component: GamesComponent},
-  {path: "game/:name", component: GameDetailComponent}
+  {path: "game/:name", component: GameDetailComponent},
+  {path: "console", component: ConsoleComponent}
 ]
 
 @NgModule({
--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { GamesComponent } from './games/games.component';
 import { GameDetailComponent } from './game-detail/game-detail.component';
-import {HttpClientModule} from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { ConsoleComponent } from './console/console.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     GamesComponent,
     GameDetailComponent,
+    ConsoleComponent,
   ],
   imports: [
     BrowserModule,
